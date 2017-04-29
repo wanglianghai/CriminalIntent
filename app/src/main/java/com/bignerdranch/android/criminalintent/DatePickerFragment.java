edd1684f0d12a068ec5c2fdc330bffc5da893abf
wanglianghai <1540446221@qq.com>
2017-04-29 11:14:53 +0800

    子fragment接收信息，calendar取date信息

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -16,6 +18,8 @@ import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
+    private Date mDate;
+    private DatePicker mDatePicker;
     public static DatePickerFragment newInstance(Date date) {
         Bundle bundle = new Bundle();
         bundle.putSerializable(ARG_DATE, date);
@@ -27,8 +31,17 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mDate = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
         //流接口
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        mDatePicker = (DatePicker) view.findViewById(R.id.select_dialog_list_view);
+        mDatePicker.init(year, month, day, null);
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
                 .setTitle(R.string.date_picker_title)
