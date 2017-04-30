a96d6b4e674ff47094a3630570f8362c7727dc8b
wanglianghai <1540446221@qq.com>
2017-04-30 09:42:15 +0800

    定义schema

---
+++ app/src/main/java/database/CrimeDbSchema/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package database.CrimeDbSchema;
+
+/**
+ * Created by Administrator on 2017/4/30/030.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable{
+        public static final String NAME = "crimes";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String Content = "content";
+            public static final String DATE = "date";
+            public static final String SOLVE = "solve";
+        }
+    }
+}
